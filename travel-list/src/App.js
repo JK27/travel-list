b5cdd49b88c4559e6023e09bf1792e45c557c55b
jk27 <jcarlosmartin27@gmail.com>
2024-06-12 14:29:13 +0100

    Added initial packing list items

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,3 +1,9 @@
+const initialItems = [
+  {id: 1, description: "Passports", quantity: 2, packed: false},
+  {id: 2, description: "Socks", quantity: 12, packed: true},
+  {id: 2, description: "Charger", quantity: 1, packed: false},
+];
+
 export default function App() {
   return <div className="app">
     <Logo />
@@ -24,7 +30,23 @@ function Form() {
 /////////////////////////////////////////////////////////// PACKING LIST
 function PackingList() {
   return (
-    <div className="list">LIST</div>
+    <div className="list" >
+      <ul>
+        {initialItems.map(item => <Item item={item} />)}
+      </ul>
+    </div>
+  );
+}
+/////////////////////////////////////////////////////////// ITEM
+function Item({item}) {
+  return (
+    <li>
+      <span style={item.packed ? {textDecoration: "line - through"} : {}}>
+        {""}
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
   );
 }
 
